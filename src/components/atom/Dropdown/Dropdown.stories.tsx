4dff3b87736d86fd972602f4c40378518b920390
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-03-09 15:00:49 +0800

    feat: atom Dropdown

---
+++ src/components/atom/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Dropdown from ".";
+
+export default {
+  title: "Dropdown",
+  component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+const dammyData = [
+  "user1",
+  "user2",
+  "user3",
+  "user4",
+  "user5",
+  "user6",
+  "user7",
+  "user7",
+  "user8",
+];
+const dammyData_empty: any[] = [];
+
+export const Primary: ComponentStory<typeof Dropdown> = () => (
+  <Dropdown
+    class="row"
+    inputStyle="large"
+    title="Repository"
+    options={dammyData_empty}
+  ></Dropdown>
+);
+
+export const Secondary: ComponentStory<typeof Dropdown> = () => (
+  <Dropdown
+    class="column"
+    inputStyle="small"
+    title="Assignee"
+    options={dammyData}
+  ></Dropdown>
+);
---
+++ src/components/atom/Dropdown/index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+
+import dropdownIcon from "../../../assets/dropdownIcon.svg";
+
+import "./style.scss";
+
+type DropdownProps = {
+  class?: string;
+  inputStyle?: string;
+  title: string;
+  options: string[];
+  optionChangeEvent?: () => void;
+};
+
+export default function Dropdown(props: DropdownProps): JSX.Element {
+  const { class: style, inputStyle, title, options } = props;
+
+  const [active, setActive] = useState(false);
+  const [selected, setSelected] = useState("My repo");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSelected("user1");
+    }, 3000);
+  });
+
+  const activeDropdown = () => {
+    setActive(active ? false : true);
+  };
+  return (
+    <div className={`dropdown ${style}`}>
+      <div className="title">{title}</div>
+      <div
+        className={`input-wrapper ${inputStyle} ${classNames(
+          active && "active"
+        )}`}
+      >
+        <button className="input" onClick={activeDropdown}>
+          <div className={classNames(selected !== "My repo" && "selected")}>
+            {selected}
+          </div>
+          <div className="icon">
+            <img src={dropdownIcon} alt="dropdownIcon" />
+          </div>
+        </button>
+        <div className="options">
+          <ul>
+            {options.length !== 0 ? (
+              options.map((option) => <li>{option}</li>)
+            ) : (
+              <li>empty</li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
