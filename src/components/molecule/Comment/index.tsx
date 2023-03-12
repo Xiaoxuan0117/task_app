33a0fff1e2c4e625124b52c3f903b8826053b763
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-03-12 20:26:58 +0800

    fix: remove redundant variable in Comment

--- src/components/molecule/Comment/index.tsx
+++ src/components/molecule/Comment/index.tsx
@@ -14,8 +14,6 @@ type ICommentProps = {
 };
 
 export default function Comment(props: ICommentProps): JSX.Element {
-  const context: string = `__Advertisement :)__\r\n\r\n- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image\r\n  resize in browser.\r\n- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly\r\n  i18n with plurals support and easy syntax.\r\n\r\nYou will like those projects!\r\n\r\n---\r\n\r\n# h1 Heading 8-)\r\n## h2 Heading\r\n### h3 Heading\r\n#### h4 Heading\r\n##### h5 Heading\r\n###### h6 Heading\r\n\r\n\r\n## Horizontal Rules\r\n\r\n___\r\n
-`;
   return (
     <div className="comment">
       <div className="avatar-wrapper">
