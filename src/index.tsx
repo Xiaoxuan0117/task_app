22fd03f4296ea16b7466afa014f0843e9e378ad2
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-03-19 00:51:01 +0800

    fix: remove strick mode to prevent uesEffect render twice

--- src/index.tsx
+++ src/index.tsx
@@ -29,11 +29,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
