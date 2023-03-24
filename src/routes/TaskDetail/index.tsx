7fbbbfbd558c3f2f23771dd5c473930b250f7f42
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-03-24 22:49:38 +0800

    style: modify TaskDetail style

--- src/routes/TaskDetail/index.tsx
+++ src/routes/TaskDetail/index.tsx
@@ -55,67 +55,71 @@ export default function TaskDetail() {
         <Navi repoOptions={repoList} />
       </div>
       <div className="task-content">
-        {isLoading && <Loading />}
-        {!errMsg && time && !isLoading && (
-          <div className="header">
-            <LinkElement
-              isRouter={false}
-              class="task"
-              href={`https://github.com/${owner}/${repo}`}
-            >
-              <div className="repo">{repo}</div>
-            </LinkElement>
-            <LinkElement isRouter={false} class="task" href={issueUrl}>
-              <div className="issue">{title}</div>
-            </LinkElement>
-            <div className="number-and-time">
-              <div className="number"># {number}</div>
-              <Time utcTime={time} />
-            </div>
-            <div className="divide"></div>
-            <div className="task-data">
-              <div className="comment-section">
-                <Comment
-                  key={id}
-                  id={id}
-                  avatar={creatorAvatar}
-                  username={creator}
-                  user_url={creatorUrl}
-                  created_at={time}
-                  allowEdit={true}
+        <div className="task-flex">
+          {isLoading && <Loading />}
+          {!errMsg && time && !isLoading && (
+            <div className="detail">
+              <div className="header">
+                <LinkElement
+                  isRouter={false}
+                  class="task"
+                  href={`https://github.com/${owner}/${repo}`}
                 >
-                  {body}
-                </Comment>
-                {commentsData.map((comment, index) => (
-                  <Comment key={comment.id} {...comment}>
-                    {comment.children}
-                  </Comment>
-                ))}
+                  <div className="repo">{repo}</div>
+                </LinkElement>
+                <LinkElement isRouter={false} class="task" href={issueUrl}>
+                  <div className="issue">{title}</div>
+                </LinkElement>
+                <div className="number-and-time">
+                  <div className="number"># {number}</div>
+                  <Time utcTime={time} />
+                </div>
               </div>
-              <div className="detail-section">
-                <TaskSidebar
-                  isOpen={isOpen}
-                  labels={labels}
-                  milestone={milestone}
-                  milestone_url={milestoneUrl}
-                  taskInfo={{
-                    owner: owner || "",
-                    repo: repo || "",
-                    number: parseInt(number || "0"),
-                  }}
-                ></TaskSidebar>
+              <div className="divide"></div>
+              <div className="task-data">
+                <div className="comment-section">
+                  <Comment
+                    key={id}
+                    id={id}
+                    avatar={creatorAvatar}
+                    username={creator}
+                    user_url={creatorUrl}
+                    created_at={time}
+                    allowEdit={true}
+                  >
+                    {body}
+                  </Comment>
+                  {commentsData.map((comment, index) => (
+                    <Comment key={comment.id} {...comment}>
+                      {comment.children}
+                    </Comment>
+                  ))}
+                </div>
+                <div className="detail-section">
+                  <TaskSidebar
+                    isOpen={isOpen}
+                    labels={labels}
+                    milestone={milestone}
+                    milestone_url={milestoneUrl}
+                    taskInfo={{
+                      owner: owner || "",
+                      repo: repo || "",
+                      number: parseInt(number || "0"),
+                    }}
+                  ></TaskSidebar>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-        {errMsg && (
-          <div className="err">
-            {errMsg}
-            <Link to="/login">
-              redirect to login page and try again{" :))"}
-            </Link>
-          </div>
-        )}
+          )}
+          {errMsg && (
+            <div className="err">
+              {errMsg}
+              <Link to="/login">
+                redirect to login page and try again{" :))"}
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
