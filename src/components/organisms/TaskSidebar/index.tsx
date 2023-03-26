b8f672b057f20e5bd6f71c0f850109f3feb7fdb1
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-03-26 23:03:37 +0800

    fix: show no labels if no labels exist

--- src/components/organisms/TaskSidebar/index.tsx
+++ src/components/organisms/TaskSidebar/index.tsx
@@ -31,9 +31,11 @@ export default function TaskSidebar(props: TaskSidebarProps): JSX.Element {
         <div className="section labels">
           <div className="title">Labels</div>
           <div className="labels">
-            {labels.map((label, index) => (
-              <Label key={`lable=${index}`}>{label}</Label>
-            ))}
+            {labels.length
+              ? labels.map((label, index) => (
+                  <Label key={`lable=${index}`}>{label}</Label>
+                ))
+              : "no labels"}
           </div>
         </div>
         <div className="section milestone">
